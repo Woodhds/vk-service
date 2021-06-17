213f385635587c55507f17c4fd20f0c23eacee5a
s.borodin <sergei.borodin@dentsu.ru>
2021-06-17 15:26:11 +0300

    pipeline

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"encoding/json"
 	"flag"
@@ -197,31 +196,33 @@ func getMessages(conn *sql.DB, id int, page int) {
 
 	posts, _ := wallClient.GetById(&reposts)
 
-	time.Sleep(time.Millisecond * 500)
-
-	var c int
-	var m *message.VkMessageModel
+	ch := make(chan *message.VkMessageModel)
 
 	if len(posts.Response.Items) > 0 {
+		go func() {
+			var c int
+			for _, post := range posts.Response.Items {
+				ch <- message.New(post, id, posts.Response.Groups)
+				c++
+			}
+			close(ch)
 
-		tran, _ := conn.BeginTx(context.Background(), &sql.TxOptions{Isolation: sql.LevelDefault})
-		for _, post := range posts.Response.Items {
+			fmt.Printf("Fetched: %d\n", c)
+		}()
 
-			m = message.New(post, id, posts.Response.Groups)
-			c = c + 1
-			_, sqlErr := tran.Exec(`
+		go func() {
+
+			for m := range ch {
+				_, sqlErr := conn.Exec(`
 			insert or ignore into messages (Id, FromId, Date, Images, LikesCount, Owner, OwnerId, RepostedFrom, RepostsCount, Text, UserReposted) 
 			values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) 
 			ON CONFLICT(id, ownerId) DO UPDATE SET LikesCount=excluded.LikesCount, RepostsCount=excluded.RepostsCount, UserReposted=excluded.UserReposted`,
-				m.ID, m.FromID, time.Time(*m.Date), strings.Join(m.Images, ";"), m.LikesCount, m.Owner, m.OwnerID, m.RepostedFrom, m.RepostsCount, m.Text, m.UserReposted)
+					m.ID, m.FromID, time.Time(*m.Date), strings.Join(m.Images, ";"), m.LikesCount, m.Owner, m.OwnerID, m.RepostedFrom, m.RepostsCount, m.Text, m.UserReposted)
 
-			if sqlErr != nil {
-				log.Print(sqlErr)
+				if sqlErr != nil {
+					log.Print(sqlErr)
+				}
 			}
-		}
-
-		tran.Commit()
+		}()
 	}
-
-	fmt.Printf("Fetched: %d\n", c)
 }
