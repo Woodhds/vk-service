bf53d5a19e0501075b44c18f27907a9535947640
s.borodin <woodhds@gmail.com>
2023-01-22 18:23:01 +0300

    return api service

--- go.mod
+++ go.mod
@@ -14,6 +14,8 @@ require (
 )
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	golang.org/x/sys v0.0.0-20220804214406-8e32c043e418 // indirect
 	golang.org/x/text v0.3.7 // indirect
--- internal/parser/service.go
+++ internal/parser/service.go
@@ -2,13 +2,20 @@ package parser
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/woodhds/vk.service/database"
 	pb "github.com/woodhds/vk.service/gen/parser"
 	"github.com/woodhds/vk.service/message"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 	"sync"
+	"time"
 )
 
 type parserImplementation struct {
@@ -73,12 +80,57 @@ func (impl *parserImplementation) Parse(ctx context.Context, _ *emptypb.Empty) (
 		}
 	}
 
+	httpClient := &http.Client{
+		Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
+		Timeout:   time.Second * 30,
+	}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go fetch(httpClient, i, postsCh, &wg)
+	}
+
 	go func() {
 		wg.Wait()
 		close(postsCh)
 	}()
 
 	return nil, nil
+}
+
+func fetch(httpClient *http.Client, page int, postsCh chan []*message.VkRepostMessage, wg *sync.WaitGroup) {
+	defer wg.Done()
+	res, err := httpClient.PostForm("https://wingri.ru/main/getPosts",
+		url.Values{
+			"page_num": []string{strconv.Itoa(page)},
+			"our":      []string{},
+			"city_id":  []string{strconv.Itoa(5)},
+		},
+	)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	doc, _ := goquery.NewDocumentFromReader(res.Body)
+	var reposts []*message.VkRepostMessage
+	doc.Find(".grid-item .post_container .post_footer a").Each(func(i int, selection *goquery.Selection) {
+		attr, exists := selection.Attr("href")
+		if exists {
+			arr := strings.Split(strings.Replace(attr, "https://vk.com/wall", "", 1), "_")
+			owner, ownerE := strconv.Atoi(arr[0])
+			id, idE := strconv.Atoi(arr[1])
+
+			if ownerE == nil && idE == nil {
+				reposts = append(reposts, &message.VkRepostMessage{
+					OwnerID: owner,
+					ID:      id,
+				})
+			}
+		}
+	})
+
+	postsCh <- reposts
 }
 
 func NewParserServer(
